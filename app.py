dac183c7b560a135133c0cbd303f445c86845904
nahiefhspc <t.al.khackheist@gmail.com>
2025-12-23 11:14:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def start_bot():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(telegram_main())
+    loop.run_until_complete(main())
 
 threading.Thread(target=start_bot, daemon=True).start()
 
